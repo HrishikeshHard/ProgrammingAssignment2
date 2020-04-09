8ad5b40e3a86d6d14d844f49141ec38893bc345f
HrishikeshHard <63086391+HrishikeshHard@users.noreply.github.com>
2020-04-09 22:43:51 +0530

    Update cachematrix.R
    R Programming Assignment 2: Lexical Scoping by Hrishikesh Hardikar

--- cachematrix.R
+++ cachematrix.R
@@ -7,21 +7,42 @@
 ##              4) getting the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invs <- NULL
+  ## Below function 'sets' the value of the matrix and deletes the old inverse (if any)
+  ## from the cache.
+  set <- function(y) {
+    x <<- y
+    invs <<- NULL
+  }
+  ## The next function will 'get' the value of the matrix.
+  get <- function() x
+  ## The next function will 'set' the value of the inverse of the matrix.
+  setInverse <- function(inverse) invs <<- inverse
+  ## The next function will 'get' the value of the inverse of the matrix.
+  getInverse <- function() invs 
+  ## Now a list should be returned with above four functions.
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 ## The next function calculates the inverse of the matrix we create using 
 ## 'makeCacheMatrix' function. But before doing so, it checks if the inverse
-## was already calculated. if yes, it skips the step and returns 'precalculated' 
-## inverse and thus saves computational power.
+## was already calculated.(This happens if there is no change made in the matrix) If yes,
+## it shows precalculated value of inverse and thus saves computational power. If no, 
+## it will calculate.
 
 
 cacheSolve <- function(x, ...) {
   invs <- x$getInverse()
   if (!is.null(invs)) {
     message("getting cache data")
-    return(invs)            ## Return a matrix that is the inverse of 'x'
+    return(invs)            
   }
+##The above steps return the value of the matrix inverse if it was already present (cached)
+##If there was no precalculated value, then the function below will calculate it AND will
+##store it -'cache' it for future reference.   
   mat <- x$get()
   invs <- solve(mat, ...)
   x$setInverse(invs)        
